57ee5d993a8b232399083c3f2cab281d8b19043b
Kabie <kabie+git@outlook.com>
2022-05-23 05:37:31 +0800

    impl ULEB128

--- lib/bcs/encoder.ex
+++ lib/bcs/encoder.ex
@@ -2,6 +2,35 @@ defmodule Bcs.Encoder do
   import Bitwise, only: [<<<: 2]
 
   @doc """
+  Unsigned Little Endian Base 128.
+
+  See https://en.wikipedia.org/wiki/LEB128#Unsigned_LEB128
+  """
+  def uleb128(value) when value >= 0 and value < unquote(1<<<7) do
+    <<value::8>>
+  end
+
+  def uleb128(value) when value >= unquote(1<<<7) and value < unquote(1<<<14) do
+    <<b1::7, b2::7>> = <<value::14>>
+    <<1::1, b2::7, 0::1, b1::7>>
+  end
+
+  def uleb128(value) when value >= unquote(1<<<14) and value < unquote(1<<<21) do
+    <<b1::7, b2::7, b3::7>> = <<value::21>>
+    <<1::1, b3::7, 1::1, b2::7, 0::1, b1::7>>
+  end
+
+  def uleb128(value) when value >= unquote(1<<<21) and value < unquote(1<<<28) do
+    <<b1::7, b2::7, b3::7, b4::7>> = <<value::28>>
+    <<1::1, b4::7, 1::1, b3::7, 1::1, b2::7, 0::1, b1::7>>
+  end
+
+  def uleb128(value) when value >= unquote(1<<<28) and value < unquote(1<<<32) do
+    <<b1::4, b2::7, b3::7, b4::7, b5::7>> = <<value::32>>
+    <<1::1, b5::7, 1::1, b4::7, 1::1, b3::7, 1::1, b2::7, 0::1, b1::7>>
+  end
+
+  @doc """
   Encode value to specific types.
   """
   def encode(value, type)
